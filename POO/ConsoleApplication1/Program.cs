0726e256e7864c9845e322b19845f848e4157d8b
jcebCarlos <jcebcarlos@gmail.com>
2017-05-02 11:41:37 +0200

    prueba de error

--- POO/ConsoleApplication1/Program.cs
+++ POO/ConsoleApplication1/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,7 +10,7 @@ namespace ConsoleApplication1
     class Program
     {
         static void Main(string[] args)
-        {
+        {fdfghfhggfh
             Console.WriteLine("Hi I'm Max");
             Console.WriteLine("Nuevo Cambio");
             Console.Read();
