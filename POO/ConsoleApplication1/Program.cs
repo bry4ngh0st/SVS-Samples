82f81f78b4c4b06302149bf2b20c53a7299bcb0b
jcebCarlos <jcebcarlos@gmail.com>
2017-05-03 08:23:17 +0200

    pruebas II

--- POO/ConsoleApplication1/Program.cs
+++ POO/ConsoleApplication1/Program.cs
@@ -14,6 +14,7 @@ namespace ConsoleApplication1
             Console.WriteLine("Hi I'm Max");
             Console.WriteLine("Nuevo Cambio");
             Console.WriteLine("prueba");
+            Console.WriteLine("-----------------");
             Console.Read();
         }
     }
