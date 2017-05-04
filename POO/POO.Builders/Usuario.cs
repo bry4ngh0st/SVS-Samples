c3507fbf1c1461ee5b7abb34dedaec0d80c0dc91
ecastaneda <edson.castaneda@cignium.com>
2017-05-04 08:15:54 -0500

    Ejemplo de constructor

---
+++ POO/POO.Builders/Usuario.cs
@@ -0,0 +1,22 @@
+﻿namespace POO.Builders
+{
+    public class Usuario
+    {
+        public string Id { get; set; }
+        public string Dni { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+
+        public Usuario()
+        {
+        }
+
+        public Usuario(string id, string dni, string nombre, string apellido)
+        {
+            Id = id;
+            Dni = dni;
+            Nombre = nombre;
+            Apellido = apellido;
+        }
+    }
+}
