44a6aee0068aa62f127685cc35536e97680c484a
bry4ngh0st <edsonbryan@gmail.com>
2017-05-04 08:37:45 -0500

    Subiendo unit tests para constructores

---
+++ POO/POO.Tests/BuilderTests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using POO.Builders;
+
+namespace POO.Tests
+{
+    [TestFixture]
+    public class BuilderTests
+    {
+        [Test]
+        public void ConstructorTest()
+        {
+            var usuario = new Usuario();
+            var otroUsuario = new Usuario("123", "70652070", "Edson", "Castañeda");
+
+            Assert.IsNull(usuario.Id);
+            Assert.IsNotNull(otroUsuario.Id);
+        }
+    }
+}
