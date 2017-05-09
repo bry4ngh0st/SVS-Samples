b05ea9269e6f4f37b810af7fa4d1625432e6994c
evelincp <evelincp.111@gmail.com>
2017-05-09 12:06:15 -0500

    Crear: UnitTest para validar Item

---
+++ TDD/TDD.UnitTests/ItemTests.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TDD.Business;
+
+namespace TDD.UnitTests
+{
+    [TestFixture]
+    public class ItemTests
+    {
+        [Test]
+        public void PruebaDeValidacion()
+        {
+            var item = new Item();
+            item.Id = 01;
+            item.Nombre = string.Empty;
+            item.Precio = 12.6m;
+            Lugar lugar = new Lugar();
+            item.Lugar = lugar;
+
+            var validar =  item.EsValido();
+
+            Assert.IsTrue(validar);
+        }
+    }  
+}
