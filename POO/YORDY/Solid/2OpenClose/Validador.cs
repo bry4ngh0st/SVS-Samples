45d6de98b10fe0dd0bcd14c32279e27271c5a741
yordy <juanyordyguillen@gmail.com>
2017-05-08 23:04:53 -0500

    arreglando

---
+++ POO/YORDY/Solid/2OpenClose/Validador.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YORDY.Solid._2OpenClose
+{
+    public class Validador
+    {
+
+        public static bool Validar(Desarrollador desarrollador) {
+            if (string.IsNullOrEmpty(desarrollador.Nombre))
+            {
+                throw new Exception(desarrollador.GetType().Name+"el nombre no puede ser nulo");
+               
+            }
+            return true;
+        }
+    }
+}
