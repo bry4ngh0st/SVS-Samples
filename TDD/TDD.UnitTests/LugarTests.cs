145f5cd413c48f365a7d98ca4ed5b67b40c14307
bry4ngh0st <edsonbryan@gmail.com>
2017-05-15 09:14:20 -0500

    [unit test] validar lugar

---
+++ TDD/TDD.UnitTests/LugarTests.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using TDD.Business;
+
+namespace TDD.UnitTests
+{
+    [TestFixture]
+    public class LugarTests
+    {
+        [Test]
+        public void ValidarLugar_DatosValidos_ReturnTrue()
+        {
+            var lugar = new Lugar
+            {
+                Id = 1,
+                Nombre = "Abancay"
+            };
+
+            Assert.IsTrue(lugar != null);
+        }
+    }
+}
