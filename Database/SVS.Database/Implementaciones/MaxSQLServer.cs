4cdd3b7edff39e1d958dc62bd4ec34b4cb27c604
Max Aguilar <Max Aguilar>
2017-05-16 09:31:40 -0500

    Implementando conexion a base de datos

--- Database/SVS.Database/Implementaciones/MaxSQLServer.cs
+++ Database/SVS.Database/Implementaciones/MaxSQLServer.cs
@@ -1,12 +1,26 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Configuration;
 
 namespace SVS.Database.Implementaciones
 {
-    class MaxSQLServer
+    public class MaxSQLServer : IDatabase
     {
+        public bool Conectar()
+        {
+            try
+            {
+                string ruta = ConfigurationManager.ConnectionStrings["db_prueba"].ConnectionString;
+                SqlConnection conexion = new SqlConnection(ruta);
+
+                Console.WriteLine("Conexion establecida:"+ conexion);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error en la conexión:"+ex.Message);
+                return false;
+            }
+        }
     }
 }
