94a08935935c08cfb709df04e2929c4e56c2ac7a
edson <edsonbryan@gmail.com>
2017-05-16 08:48:37 -0500

    Entidades de base de datos

---
+++ Frameworks/SVS.Frameworks.Data.EF/DatabaseContext.cs
@@ -0,0 +1,15 @@
+﻿using SVS.Frameworks.Data.EF.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SVS.Frameworks.Data.EF
+{
+    public class DatabaseContext : DbContext
+    {
+        public DbSet<Item> Items { get; set; }
+    }
+}
