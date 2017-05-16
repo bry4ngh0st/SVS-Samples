07c3fc32f763467aaa51f332f1517078c783e823
Arthur <artuco12@gmail.com>
2017-05-16 09:54:22 -0500

    Subiendo Conexion final.

--- Database/SVS.Database/Implementaciones/ArthurSQLServer.cs
+++ Database/SVS.Database/Implementaciones/ArthurSQLServer.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace SVS.Database.Implementaciones
 {
@@ -10,7 +7,22 @@ namespace SVS.Database.Implementaciones
     {
         public bool Conectar()
         {
-            
+            System.Data.SqlClient.SqlConnection conn = new System.Data.SqlClient.SqlConnection();
+            conn.ConnectionString = "integrated security=SSPI;data source=;" + "persist security info=False;initial catalog=northwind";
+            try
+            {
+                conn.Open();
+                return true;
+                // Insert code to process data.
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+            finally
+            {
+                conn.Close();
+            }
         }
     }
 }
