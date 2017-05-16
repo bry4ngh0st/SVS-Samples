d308375bb8b3953d1cd15746d5c2b9a188fc0416
evelincp <evelincp.111@gmail.com>
2017-05-16 11:51:38 -0500

    Implementando conexion en EveSQLServer

--- Database/SVS.Database/Implementaciones/EveSQLServer.cs
+++ Database/SVS.Database/Implementaciones/EveSQLServer.cs
@@ -3,10 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.Sql;
+using System.Configuration;
+using System.Data.SqlClient;
+using System;
 
 namespace SVS.Database.Implementaciones
 {
-    class EveSQLServer
+    class EveSQLServer: IDatabase
     {
+        public bool Conectar()
+        {
+            try
+            {
+                var conexionString = ConfigurationManager.ConnectionStrings["dbprueba"].ConnectionString;
+                using (var sqlConnection = new SqlConnection(conexionString))
+                {
+                    sqlConnection.Open();
+                    sqlConnection.Close();
+                }
+
+                return true;
+            }
+            catch(Exception ex)
+            {
+                throw new Exception($"Error en la conexion a la base de datos. {ex.Message}");
+            }
+        }
     }
 }
