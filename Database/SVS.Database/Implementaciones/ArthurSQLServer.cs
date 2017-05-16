bdc3cf788bc85e35decf765b71784295bc2e6929
Arthur <artuco12@gmail.com>
2017-05-16 09:32:16 -0500

    Implementando la Conexion a la base de datos ArthurSQLServer.

---
+++ Database/SVS.Database/Implementaciones/ArthurSQLServer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SVS.Database.Implementaciones
+{
+    public class ArthurSQLServer : IDatabase
+    {
+        public bool Conectar()
+        {
+            
+        }
+    }
+}
