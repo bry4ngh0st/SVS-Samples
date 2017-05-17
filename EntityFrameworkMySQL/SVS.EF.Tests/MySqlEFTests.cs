39ab89f0edd0598d9796db4c891880588d2d55a5
edson <edsonbryan@gmail.com>
2017-05-17 11:49:01 -0500

    agregando datos a Post mediante unit test

--- EntityFrameworkMySQL/SVS.EF.Tests/MySqlEFTests.cs
+++ EntityFrameworkMySQL/SVS.EF.Tests/MySqlEFTests.cs
@@ -1,17 +1,44 @@
 ﻿using NUnit.Framework;
+using SVS.EF.Entidades;
 using System;
+using System.Collections.Generic;
 
 namespace SVS.EF.Tests
 {
     [TestFixture]
     public class MySqlEFTests
     {
+        private static ContextoBD _contexto = new ContextoBD();
+
+        [Test]
+
+        public void RegistrarNuevosPostsTest()
+        {
+            var posts = new List<Post>()
+            {
+                new Post { Contenido = "Post sobre entity framework", FechaRegistro = DateTime.Now },
+                new Post { Contenido = "Post sobre errores en entity framework", FechaRegistro = DateTime.Now },
+                new Post { Contenido = "Post sobre principios solid", FechaRegistro = DateTime.Now },
+                new Post { Contenido = "Post sobre patrones de diseno", FechaRegistro = DateTime.Now },
+                new Post { Contenido = "UNAMBA", FechaRegistro = DateTime.Now },
+                new Post { Contenido = "Spring Valley Software", FechaRegistro = DateTime.Now },
+                new Post { Contenido = "Kevin", FechaRegistro = DateTime.Now },
+                new Post { Contenido = "Percy", FechaRegistro = DateTime.Now },
+                new Post { Contenido = "Post sobre entity framework. Parte 2", FechaRegistro = DateTime.Now }
+            };
+
+            foreach (var post in posts)
+            {
+                _contexto.Posts.Add(post);
+            }
+
+            _contexto.SaveChanges();
+        }
+
         [Test]
         public void GetPostsTest()
         {
-            var context = new ContextoBD();
-
-            var posts = context.Posts;
+            var posts = _contexto.Posts;
 
             foreach (var post in posts)
             {
