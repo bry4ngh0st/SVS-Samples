02333009c47a412db0892b5869df8dc3e353159a
Arthur <artuco12@gmail.com>
2017-05-17 08:35:26 -0500

    Agregando Modificaciones realizadas en el Archivo A.

---
+++ Database/SVS.Database.EF.AE.Entities/Post.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SVS.Database.EF.AE.Entities
+{
+    public class Post
+    {
+        public int Id { get; set; }
+        public string Titulo { get; set; }
+        public string Contenido { get; set; }
+        public DateTime FechaRegistro { get; set; }
+        public DateTime FechaModificacion { get; set; }
+    }
+}
---
+++ Database/SVS.Database.EF.AE/DatabaseContext.cs
@@ -0,0 +1,12 @@
+﻿using SVS.Database.EF.AE.Entities;
+using System.Data.Entity;
+namespace SVS.Database.EF.AE
+{
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext() : base("prueba")
+        {
+        }
+        public DbSet<Post> ListaPost { get; set; }
+    }
+}
