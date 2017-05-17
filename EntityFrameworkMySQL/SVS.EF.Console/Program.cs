e39d050e3fe8ec9308190f8045405ebf9155c95e
yordy <juanyordyguillen@gmail.com>
2017-05-17 13:03:56 -0500

    ultimo

--- EntityFrameworkMySQL/SVS.EF.Console/Program.cs
+++ EntityFrameworkMySQL/SVS.EF.Console/Program.cs
@@ -16,14 +16,42 @@ namespace SVS.EF.Console
             //RegistrarPosts();
             //System.Console.WriteLine("Posts agregados correctamente");
 
-            var posts = _contexto.Posts;
+            //var idPos = 3;
+            //var datooptenido= ObtenerPostSegunID(idPos);           
 
-            foreach (var post in posts)
+            //var posts = _contexto.Posts;
+            //System.Console.WriteLine("obtener id post");
+            //foreach (var post in posts)
+            //{
+            //    System.Console.WriteLine($"{post.Id} | {post.Contenido}");
+            //}
+
+            //var mostrar= UltimoPostRegistrado();
+            // System.Console.WriteLine($"{mostrar.Id},{mostrar.Contenido}");  
+            var buscarPalabra = "mvc";
+            var listaPosts = ContenidoSegunPalabraEspecifica(buscarPalabra);
+            foreach (var listaPost in listaPosts )
             {
-                System.Console.WriteLine($"{post.Id} | {post.Contenido}");
+                System.Console.WriteLine($"{listaPost.Id},{listaPost.Contenido}");
             }
 
             System.Console.ReadLine();
+        }
+        static public Post UltimoPostRegistrado()
+        {
+            var ultimo=_contexto.Posts.ToList();
+            var post = ultimo.Last();
+            return post;
+        }
+        static public Post ObtenerPostSegunID(int id)
+        {
+            var posID=_contexto.Posts.Find(id);
+            return posID;
+        }
+        static public List<Post> ContenidoSegunPalabraEspecifica(string palabra)
+        {
+            var posts = _contexto.Posts.Where(p => p.Contenido.Contains(palabra));
+            return posts.ToList();
         }
 
         private static void RegistrarPosts()
@@ -37,7 +65,8 @@ namespace SVS.EF.Console
                 new Post{Contenido = "Contenido del post 5", FechaRegistro = DateTime.Now },
                 new Post{Contenido = "Contenido del post 6", FechaRegistro = DateTime.Now },
                 new Post{Contenido = "Contenido del post 7", FechaRegistro = DateTime.Now },
-                new Post{Contenido = "Contenido del post 8", FechaRegistro = DateTime.Now }
+                new Post{Contenido = "Contenido del post 8", FechaRegistro = DateTime.Now },
+                new Post{Contenido = "Contenido del post 9", FechaRegistro = DateTime.Now }
             };
 
             foreach (var post in posts)
@@ -45,7 +74,7 @@ namespace SVS.EF.Console
                 _contexto.Posts.Add(post);
             }
 
-            _contexto.SaveChanges();            
+            _contexto.SaveChanges();
         }
     }
 }
