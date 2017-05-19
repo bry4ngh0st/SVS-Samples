eed814d53b3ff44a3037fe8084029a2bf1556a04
yordy <juanyordyguillen@gmail.com>
2017-05-19 11:41:20 -0500

    MVC

---
+++ EntityFrameworkMySQL/SVS.EF.UI/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SVS.EF.UI.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "MVc";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "nuevo.";
+
+            return View();
+        }
+    }
+}
---
+++ EntityFrameworkMySQL/SVS.EF.UI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SVS.EF.UI.MvcApplication" Language="C#" %>
